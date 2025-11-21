d63174fd58ba8fa57e28e7e68a03ad0d0fc2d6eb
omerfarukaktulum <omerfaruk.aktulum@gmail.com>
2025-11-21 17:59:00 +0100

    Update navbar label from Eküri to Ekürim

--- lib/constants/tr.ts
+++ lib/constants/tr.ts
@@ -46,7 +46,7 @@ export const TR = {
     expenses: 'Giderler',
     notes: 'Notlar',
     statistics: 'İstatistikler',
-    stablemate: 'Eküri',
+    stablemate: 'Ekürim',
     billing: 'Faturalandırma',
     settings: 'Ayarlar',
     admin: 'Yönetim',
